9c4d06c841133a43e559b48f3167bb536f9c439b
Aaron <aaronkikabhai97@gmail.com>
2023-03-18 22:02:46 +0000

    Remove sourcemap and add srcDir path

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,19 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const srcDir = resolve(__dirname, 'src');
 
 export default defineConfig({
   build: {
     rollupOptions: {
       plugins: [react({ fastRefresh: false })],
       input: {
-        index: resolve(__dirname, 'src/index.ts'),
-        popup: resolve(__dirname, 'src/popup/index.html'),
+        index: resolve(srcDir, 'index.ts'),
+        popup: resolve(srcDir, 'popup/index.html'),
       },
       output: {
         entryFileNames: (chunk) => `${chunk.name}.js`,
       },
     },
-    sourcemap: !isProduction,
   },
 });
